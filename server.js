35838a7b9791763504329dcf1cb4765059b33267
Luiz M <luiz.meneghim@hotmail.com>
2022-09-04 13:39:07 -0700

    server setup

--- server.js
+++ server.js
@@ -8,4 +8,43 @@ const flash = require('express-flash');
 const logger = require('morgan');
 const connectDB = require('./config/database');
 
+// Routing
+
+// Dotenv setup
 require('dotenv').config({ path: './config/.env' });
+
+connectDB();
+
+// Server Middleware setup
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(logger('dev'));
+
+// Sessions
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+
+// Passport config
+// require('./config/passport')(passport);
+
+// Passport middleware
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+app.use(flash());
+
+// add routesd here
+//
+
+// App port listen
+app.listen(process.env.PORT, () => {
+  console.log('Server is running, you better catch it!');
+});
